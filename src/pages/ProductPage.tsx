540de747bce1a0e5c0e47bfa850eda7cb0a785f4
salman854raza <raza3213710@gmail.com>
2024-12-27 07:16:59 -0800

    update the code2

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -45,7 +45,7 @@ const productTabs = [
   },
 ];
 
-export const ProductPage: React.FC = () => {
+ const ProductPage: React.FC = () => {
   return (
     <div className="min-h-screen bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -65,3 +65,4 @@ export const ProductPage: React.FC = () => {
     </div>
   );
 }
+export default ProductPage;
